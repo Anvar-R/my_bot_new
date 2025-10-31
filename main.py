9324b2cea9c87fc4a6efe8230cbfe5ac7edff38a
Anvar Rustamov <anvarrustamov@list.ru>
2025-10-31 17:28:39 +0500

    'env'

---
+++ main.py
@@ -0,0 +1,9 @@
+import logging
+import dotenv
+
+dotenv.load_dotenv()
+logging.basicConfig(
+    level=logging.DEBUG,
+    format='[%(asctime)s] #%(levelname)-8s %(filename)s:'
+           '%(lineno)d - %(name)s - %(message)s'
+)
