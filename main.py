24a1bbda4ddc6b2de15f7f84140a3831045fc028
Anvar Rustamov <anvarrustamov@list.ru>
2025-11-14 17:01:43 +0500

    'config'

--- main.py
+++ main.py
@@ -1,10 +1,49 @@
+import asyncio
 import logging
 from config.config import Config, load_config
+from aiogram import Bot, Dispatcher
+from aiogram.client.default import DefaultBotProperties
+from aiogram.enums import ParseMode
 
 config: Config = load_config()
+logger = logging.getLogger(__name__)
 
-logging.basicConfig(
-    level=logging.DEBUG,
-    format='[%(asctime)s] #%(levelname)-8s %(filename)s:'
-           '%(lineno)d - %(name)s - %(message)s'
-)
+
+async def main():
+    logging.basicConfig(
+            level=logging.DEBUG,
+            format='[%(asctime)s] #%(levelname)-8s %(filename)s:'
+            '%(lineno)d - %(name)s - %(message)s'
+    )
+
+    config: Config = load_config()
+    bot = Bot(
+        token=config.bot.token,
+        default=DefaultBotProperties(parse_mode=ParseMode.HTML)
+    )
+
+    dp = Dispatcher()
+
+    # Инициализируем другие объекты (пул соединений с БД, кеш и т.п.)
+    # ...
+
+    # Помещаем нужные объекты в workflow_data диспетчера
+    #dp.workflow_data.update(...)
+
+    # Настраиваем главное меню бота
+    #await set_main_menu(bot)
+
+    # Регистриуем роутеры
+    logger.info('Подключаем роутеры')
+    # ...
+
+    # Регистрируем миддлвари
+    logger.info('Подключаем миддлвари')
+    # ...
+
+    # Пропускаем накопившиеся апдейты и запускаем polling
+    #await bot.delete_webhook(drop_pending_updates=True)
+    await dp.start_polling(bot)
+
+
+asyncio.run(main())
